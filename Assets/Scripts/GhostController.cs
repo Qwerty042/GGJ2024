7c2f2652978c804261c2c77302153893d63197d2
unknown <GameJam@LAB-RG19-01.resource.waikato.ac.nz>
2024-01-27 21:41:53 +1300

    cool dying effect but i hope you have plenty of memory

--- Assets/Scripts/GhostController.cs
+++ Assets/Scripts/GhostController.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GhostController : MonoBehaviour
 {
+    float alpha = 1.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,8 @@ public class GhostController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        GetComponent<SpriteRenderer>().color = new Color(1.0f, 1.0f, 1.0f, alpha);
+        transform.position += new Vector3(0, 1.0f * Time.deltaTime);
+        alpha -= 1.0f * Time.deltaTime;
     }
 }
